d0c43aca81d9318949c10e8d671964ff51d038e8
namiki <namiki081025@gmail.com>
2024-11-22 20:16:28 +0900

    [update]配列のmap,filterメソッドについて

--- 03_js_basic/047_array_method/start/main.js
+++ 03_js_basic/047_array_method/start/main.js
@@ -1,2 +1,21 @@
 const arry = [10, 20, 30, 40];
 const newArry = [];
+
+for (let i = 0; i < arry.length; i++) {
+  const val = arry[i] * 2;
+  if (val > 50) {
+    newArry.push(val);
+  }
+}
+
+console.log(newArry);
+
+const newArry2 = arry.map((val) => val * 2);
+const newArry3 = newArry2.filter((val) => val > 50);
+
+console.log(newArry3);
+console.log(newArry2);
+
+const newArry4 = arry.map((val) => val * 2).filter((val) => val > 50);
+
+console.log(newArry4);
