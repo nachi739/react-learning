b841c8057b9b0124de680e24a381fefa43d1db9e
namiki <namiki081025@gmail.com>
2024-12-11 18:24:35 +0900

    [update]コンポーネントの分割方法

---
+++ 04_react_basic/src/070_component_nest/start/components/Child.jsx
@@ -0,0 +1,13 @@
+import "./Child.css";
+import { List } from "./List";
+
+const Child = () => {
+  return (
+    <div className="component">
+      <h3>Hello Child</h3>
+      <List />
+    </div>
+  );
+};
+
+export default Child;
