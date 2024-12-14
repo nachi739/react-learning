56a018bc9990eaaecbc4a36fb3f7c3f868e978de
namiki <namiki081025@gmail.com>
2024-12-14 16:22:11 +0900

    [update]Fragmentの使い方

--- 04_react_basic/src/075_fragment/start/components/Child.jsx
+++ 04_react_basic/src/075_fragment/start/components/Child.jsx
@@ -1,10 +1,19 @@
 import "./Child.css";
+import React from "react";
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <React.Fragment>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nulla ullam
+        ab deleniti velit natus laudantium officia quos earum nihil odit
+        voluptas nesciunt, dignissimos ipsam rem iusto saepe eos ad.
+      </p>
+    </React.Fragment>
   );
 };
 
