973b7f261b6a5c9dd21834ecd3dd4b3a5d63d591
namiki <namiki081025@gmail.com>
2024-12-24 22:09:31 +0900

    [update]propsでコンポーネントに値を渡してみる

--- 04_react_basic/src/090_props/start/components/Child.jsx
+++ 04_react_basic/src/090_props/start/components/Child.jsx
@@ -5,9 +5,10 @@
 
 import "./Child.css";
 
-const Child = () => {
+//const Child = (props) => {
+const Child = ({ color }) => {
   return (
-    <div className="component">
+    <div className={`component ${color}`}>
       <h3>Hello Component</h3>
     </div>
   );
